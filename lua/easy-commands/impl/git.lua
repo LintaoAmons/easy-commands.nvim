36f344e002193d1fffa04de1f1a73d7251f3383a
Lintao <Lintao.Amons@outlook.com>
2023-06-18 17:53:28 +0800

    [feat] add more git commands

--- lua/easy-commands/impl/git.lua
+++ lua/easy-commands/impl/git.lua
@@ -1,16 +1,19 @@
 local M = {}
 
 M.GitDiff = "DiffviewOpen"
--- M.GitCommit =
 M.GitStatus = "FzfLua git_status"
 M.GitStash = "!git stash"
 M.GitStashPop = "!git stash pop"
 M.GitPush = "!git push"
+-- M.GitCommit = TODO: 
 M.GitListCommits = "DiffviewFileHistory"
 M.GitListCurrentFileCommits = "DiffviewFileHistory %"
 M.GitNextHunk = "lua require 'gitsigns'.next_hunk({navigation_message = false})"
 M.GitPrevHunk = "lua require 'gitsigns'.prev_hunk({navigation_message = false})"
--- M.GitResetHunk =
-M.GitListBranches =  "FzfLua git_branches"
+M.GitResetHunk = "lua require 'gitsigns'.reset_hunk()"
+M.GitListBranches = "FzfLua git_branches"
+M.BlameLine = "lua require 'gitsigns'.blame_line()"
+M.ResetBuffer = "lua require 'gitsigns'.reset_buffer()"
+M.GitLazygit = "lua require 'lvim.core.terminal'.lazygit_toggle()"
 
 return M
