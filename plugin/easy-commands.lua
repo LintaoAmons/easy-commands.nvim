3a31e2c0f0cf4ffcdc2e49bea326c9585546fb63
Lintao <lintao.amons@outlook.com>
2024-01-21 14:03:20 +0800

    fix: plugin/easy-commands.lua

--- plugin/easy-commands.lua
+++ plugin/easy-commands.lua
@@ -1,23 +1,12 @@
 if vim.fn.has("nvim-0.7.0") == 0 then
-	vim.api.nvim_err_writeln("scratch requires at least nvim-0.7.0.1")
-	return
+  vim.api.nvim_err_writeln("easy-commands requires at least nvim-0.7")
+  return
 end
 
 -- make sure this file is loaded only once
 if vim.g.loaded_easy_commands == 1 then
-	return
+  return
 end
 vim.g.loaded_easy_commands = 1
 
-
-local function call_command_with_fail_hints(cmd)
-	local isOk, _ = pcall(vim.cmd, "Lspsaga rename")
-	if not isOk then
-		vim.notify(
-			"Failed to call command `Rename`\nCheck dependency `Lspsaga`",
-			vim.log.levels.ERROR,
-			{ title = "easy-commands.nvim" }
-		)
-	end
-end
-
+require("easy-commands").setup()
